548e9efddbbbe616369292def31d11d558dc9da6
JanMalawski <janmalawski2@gmail.com>
2019-03-18 17:56:04 +0100

    Created Student class with CreateStudent and ShowStudent methods. Used Student class in Program.cs

--- Main/Program.cs
+++ Main/Program.cs
@@ -10,6 +10,21 @@ namespace Main
     {
         static void Main(string[] args)
         {
+            Student student1 = new Student();
+            Console.WriteLine("Insert Name:");
+            student1.SetName(Console.ReadLine());
+            Console.WriteLine("Insert Surname:");
+            student1.SetSurname(Console.ReadLine());
+            Console.WriteLine("Insert Age:");
+            student1.SetAge(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Insert Class:");
+            student1.SetClass(Console.ReadLine());
+
+            
+            student1.ShowStudent();
+            //Console.WriteLine("Insert student's name:");
+            //
+            //Console.ReadLine();
         }
     }
 }
---
+++ Main/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class Student
+    {
+        string Name;
+        string Surname;
+        int Age;
+        string Class;
+
+        public void SetName(string N)
+        {
+            Name = N;
+        }
+        public void SetSurname(string S)
+        {
+            Surname = S;
+        }
+        public void SetAge(int A)
+        {
+            Age = A;
+        }
+        public void SetClass(string C)
+        {
+            Class = C;
+        }
+
+
+            public void ShowStudent()
+        {
+            Console.WriteLine("Info about student:");
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Surname: " + Surname);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Class: " + Class);
+            
+        }
+    }
+}
