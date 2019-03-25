485ac04df7bbf065b8facb79baf56c562bd1cb63
JanMalawski <janmalawski2@gmail.com>
2019-03-25 14:13:10 +0100

    Created Menus class and moved DisplayLibraryMenu and DisplayStudentMenu methods to it.

--- Main/Program.cs
+++ Main/Program.cs
@@ -8,65 +8,6 @@ namespace Main
 {
     class Program
     {
-        static public int DisplayLibraryMenu()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Welcome to Library");
-            Console.WriteLine();
-            Console.WriteLine("1. Create a student account");
-            Console.WriteLine("2. Add a book to the library");
-            Console.WriteLine("3. Check for student");
-            Console.WriteLine("4. Check for book");
-            Console.WriteLine("5. Exit");
-            Console.WriteLine();
-            int result = 0;
-            while (result == 0)
-            {
-                try
-                {
-                    result = Convert.ToInt32(Console.ReadLine());
-                  
-                }
-                catch(FormatException)
-                {
-                    Console.WriteLine("Please enter number that corresponds with option.");
-                }
-            }
-           
-            return Convert.ToInt32(result);
-        }
-        static public int DisplayStudentsMenu()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Students' List menu:");
-            Console.WriteLine();
-            Console.WriteLine("1. Show all records");
-            Console.WriteLine("2. Find by ID");
-            Console.WriteLine("3. Find by name");
-            Console.WriteLine("4. Find by surname");
-            Console.WriteLine("5. Find by age");
-            Console.WriteLine("6. Find by class (eg. 1A or 3B)");
-            Console.WriteLine("7. Exit");
-            Console.WriteLine();
-            int result3 = 0;
-            while (result3 == 0)
-            {
-                try
-                {
-                    result3 = Convert.ToInt32(Console.ReadLine());
-
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Please enter number that corresponds with option.");
-                }
-            }
-
-            return Convert.ToInt32(result3);
-        }
-
-
-
         static void Main(string[] args)
         {
 
@@ -156,14 +97,14 @@ namespace Main
                             }
                             Console.ReadKey();
                         }
-                        userInputStudentsMenu = DisplayStudentsMenu();
+                        userInputStudentsMenu = Menus.DisplayStudentsMenu();
                     } 
                     while (userInputStudentsMenu != 7);
                     
                     
                 }
 
-                userInputLibraryMenu = DisplayLibraryMenu();
+                userInputLibraryMenu = Menus.DisplayLibraryMenu();
                 
             } while (userInputLibraryMenu != 5);
             
